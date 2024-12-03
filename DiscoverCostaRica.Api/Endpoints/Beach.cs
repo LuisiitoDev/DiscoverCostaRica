5a91a85a89086df45cb92dab94fd8d4f79e624b7
luissandovalcode2023 <luis.sandoval@denovostudios.com>
2024-12-03 16:54:12 -0600

    mapping endpoint issue

--- DiscoverCostaRica.Api/Endpoints/Beach.cs
+++ DiscoverCostaRica.Api/Endpoints/Beach.cs
@@ -13,8 +13,6 @@ public class Beach : IEndpoint
         app.MapGet(EndpointRoutes.BEACH_ROUTES.GET_BEACHES, GetBeaches)
             .WithName("GetBeaches")
             .WithOpenApi(OpenApiBeach.GetBeach)
-            .WithApiVersionSet(VersioningConfiguration.GetVersion(app))
-            .MapToApiVersion(2, 0)
             .Produces<DtoBeach[]>(StatusCodes.Status200OK);
     }
 
