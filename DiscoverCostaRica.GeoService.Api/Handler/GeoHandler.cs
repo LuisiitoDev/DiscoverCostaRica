4b343baa49a74c056ba2a31ffe9816fe56b979a9
LuisiitoDev <luisasandovalcoder@gmail.com>
2025-09-28 21:36:55 -0600

    add geo microservice

---
+++ DiscoverCostaRica.GeoService.Api/Handler/GeoHandler.cs
@@ -0,0 +1,26 @@
+﻿using DiscoverCostaRica.Geo.Application.Interfaces;
+using DiscoverCostaRica.Shared.Utils;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DiscoverCostaRica.Geo.Api.Handler;
+
+public static class GeoHandler
+{
+    public static async Task<IResult> GetProvinces([FromServices] IGeoService service, CancellationToken cancellationToken)
+    {
+        var result = await service.GetProvinces(cancellationToken);
+        return result.ToResult();
+    }
+
+    public static async Task<IResult> GetCantonsByProvince([FromQuery] int provinceId, [FromServices] IGeoService service, CancellationToken cancellationToken)
+    {
+        var result = await service.GetCantons(provinceId, cancellationToken);
+        return result.ToResult();
+    }
+
+    public static async Task<IResult> GetDistrictsByCanton([FromQuery] int cantonId, [FromServices] IGeoService service, CancellationToken cancellationToken)
+    {
+        var result = await service.GetDistricts(cantonId, cancellationToken);
+        return result.ToResult();
+    }
+}
---
+++ DiscoverCostaRica.GeoService.Api/Profiles/MappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using DiscoverCostaRica.Geo.Application.Dtos;
+using DiscoverCostaRica.Geo.Domain.Models;
+
+namespace DiscoverCostaRica.Geo.Api.Profiles;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<ProvinceDto, ProvinceModel>().ReverseMap();
+        CreateMap<CantonDto, CantonModel>().ReverseMap();
+        CreateMap<DistrictDto, DistrictModel>().ReverseMap();
+    }
+}
