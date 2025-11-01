252bbac8d4c3e81ba1750003f446cff7e9bba048
LuisiitoDev <luisasandovalcoder@gmail.com>
2025-11-01 16:30:06 -0600

    Align project dependencies with database readiness
    Updated `discovercostarica-beaches-api`, `discovercostarica-cultureserice-api`, and `discovercostarica-volcanoservice-api` to reference the `db` database and wait for its readiness. These changes ensure consistency across all projects, aligning them with the existing behavior of `discovercostarica-geoservice-api`. This improves dependency management and application reliability by ensuring the database is available before project initialization.

--- DiscoverCostaRica.AppHost/AppHost.cs
+++ DiscoverCostaRica.AppHost/AppHost.cs
@@ -3,14 +3,20 @@ var builder = DistributedApplication.CreateBuilder(args);
 var sql = builder.AddSqlServer("sql").WithLifetime(ContainerLifetime.Persistent);
 var db = sql.AddDatabase("DiscoverCostaRica");
 
-builder.AddProject<Projects.DiscoverCostaRica_Beaches_Api>("discovercostarica-beaches-api");
+builder.AddProject<Projects.DiscoverCostaRica_Beaches_Api>("discovercostarica-beaches-api")
+       .WithReference(db)
+       .WaitFor(db);
 
-builder.AddProject<Projects.DiscoverCostaRica_Culture_Api>("discovercostarica-cultureserice-api");
+builder.AddProject<Projects.DiscoverCostaRica_Culture_Api>("discovercostarica-cultureserice-api")
+       .WithReference(db)
+       .WaitFor(db);
 
 builder.AddProject<Projects.DiscoverCostaRica_Geo_Api>("discovercostarica-geoservice-api")
        .WithReference(db)
        .WaitFor(db);
 
-builder.AddProject<Projects.DiscoverCostaRica_VolcanoService_Api>("discovercostarica-volcanoservice-api");
+builder.AddProject<Projects.DiscoverCostaRica_VolcanoService_Api>("discovercostarica-volcanoservice-api")
+       .WithReference(db)
+       .WaitFor(db);
 
 builder.Build().Run();
