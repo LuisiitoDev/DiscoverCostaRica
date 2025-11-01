d57e6b74d0aaa8cae221a88c902e630999b42534
LuisiitoDev <luisasandovalcoder@gmail.com>
2025-11-01 00:06:16 -0600

    Add volcano API endpoints and handler logic
    Introduced a global using directive in `GlobalUsings.cs` to simplify access to shared extensions. Added `EndpointExtensions` to define and map volcano-related API routes under `api/v1/volcano`.
    
    Implemented `VolcanoHandler` to handle the logic for:
    - Retrieving all volcanoes (`GET /`).
    - Retrieving a volcano by ID (`GET /Volcano?id={id}`).
    - Retrieving volcanoes by province ID (`GET /Volcano/Province?id={id}`).
    
    These changes improve modularity, maintainability, and scalability.

---
+++ src/DiscoverCostaRica.Volcano/DiscoverCostaRica.VolcanoService.Api/Handler/VolcanoHandler.cs
@@ -0,0 +1,25 @@
+﻿using DiscoverCostaRica.VolcanoService.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DiscoverCostaRica.VolcanoService.Api.Handler;
+
+public static class VolcanoHandler
+{
+    public static async Task<IResult> GetVolcanos([FromServices] IVolcanoService service, CancellationToken cancellationToken)
+    {
+        var result = await service.GetVolcanos(cancellationToken);
+        return result.ToResult();
+    }
+
+    public static async Task<IResult> GetVolcanoById([FromQuery] int id, [FromServices] IVolcanoService service, CancellationToken cancellationToken)
+    {
+        var result = await service.GetVolcanoById(id, cancellationToken);
+        return result.ToResult();
+    }
+
+    public static async Task<IResult> GetVolcanosByProvince([FromQuery] int id, [FromServices] IVolcanoService service, CancellationToken cancellationToken)
+    {
+        var result = await service.GetVolcanosByProvince(id, cancellationToken);
+        return result.ToResult();
+    }
+}
