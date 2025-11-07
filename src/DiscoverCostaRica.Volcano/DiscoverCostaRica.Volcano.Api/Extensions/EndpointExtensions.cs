387720759d678251ddec72772f4345bb205d7a94
LuisiitoDev <luisasandovalcoder@gmail.com>
2025-11-06 22:02:44 -0600

    Add API versioning to VolcanoService endpoints
    Updated `using` directives to include `DiscoverCostaRica.Shared.ApiVersioning` for API versioning support. Modified `MapVolcanoEndpoints` to use a versioned route pattern (`/api/v{version:apiVersion}/volcanoes`) and introduced a `versionSet` with `CreateGlobalVersionSet`. Associated the endpoint group with the version set using `WithApiVersionSet` and explicitly mapped endpoints to API version 1.0 using `MapToApiVersion(1.0)`.

--- src/DiscoverCostaRica.Volcano/DiscoverCostaRica.Volcano.Api/Extensions/EndpointExtensions.cs
+++ src/DiscoverCostaRica.Volcano/DiscoverCostaRica.Volcano.Api/Extensions/EndpointExtensions.cs
@@ -1,4 +1,5 @@
-﻿using DiscoverCostaRica.VolcanoService.Api.Handler;
+﻿using DiscoverCostaRica.Shared.ApiVersioning;
+using DiscoverCostaRica.VolcanoService.Api.Handler;
 
 namespace DiscoverCostaRica.VolcanoService.Api.Extensions;
 
@@ -6,7 +7,10 @@ public static class EndpointExtensions
 {
     public static IEndpointRouteBuilder MapVolcanoEndpoints(this IEndpointRouteBuilder endpoints)
     {
-        var groups = endpoints.MapGroup("api/v1/volcanoes");
+        var versionSet = endpoints.CreateGlobalVersionSet();
+        var groups = endpoints.MapGroup("/api/v{version:apiVersion}/volcanoes")
+                              .WithApiVersionSet(versionSet)
+                              .MapToApiVersion(1.0);
 
         groups.MapGet("/", VolcanoHandler.GetVolcanos);
         groups.MapGet("/{id}", VolcanoHandler.GetVolcanoById);
