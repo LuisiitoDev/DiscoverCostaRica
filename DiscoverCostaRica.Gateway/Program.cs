47dbb0973ddb28573a300eb88ec02788fb126a24
LuisiitoDev <luisasandovalcoder@gmail.com>
2025-11-20 20:21:04 -0600

    Add request transform for Authorization header
    Enhanced the reverse proxy by adding a transformation to forward
    the `Authorization` header from incoming requests to outgoing
    proxy requests. This was achieved by using the YARP
    `AddTransforms` method. The transformation extracts the token
    value from the `Authorization` header (removing the "Bearer "
    prefix) and sets it in the outgoing request. Updated the
    `AddServiceDiscoveryDestinationResolver` method chaining to
    include the new transformation logic.

--- DiscoverCostaRica.Gateway/Program.cs
+++ DiscoverCostaRica.Gateway/Program.cs
@@ -1,3 +1,5 @@
+using Yarp.ReverseProxy.Transforms;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.AddServiceDefaults();
@@ -5,7 +7,19 @@ builder.AddServiceDefaults();
 builder.Services
     .AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"))
-    .AddServiceDiscoveryDestinationResolver();
+    .AddServiceDiscoveryDestinationResolver()
+    .AddTransforms(transforms =>
+    {
+        transforms.AddRequestTransform(async context =>
+        {
+            if (context.HttpContext.Request.Headers.TryGetValue("Authorization", out var token))
+            {
+                const string headerName = "Bearer ";
+                var tokenValue = token.ToString()[headerName.Length..].Trim();
+                context.ProxyRequest.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", tokenValue);
+            }
+        });
+    });
 
 var app = builder.Build();
 
